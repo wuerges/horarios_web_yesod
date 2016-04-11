211a88b7ab3af89a6b5f1b032dd5aa3d5f9ed073
Emilio Wuerges <wuerges@gmail.com>
2016-04-11 15:06:05 -0300

    ok

--- Handler/Professor.hs
+++ Handler/Professor.hs
@@ -24,14 +24,35 @@ faseField = checkBool checkN errorMessage intField
   where errorMessage = "Fase Inválida" :: Text
         checkN n = (n > 0) && (n <= 10)
 
+--renderFormFase :: Entity Fase -> Handler Html
+generateFormFase (Entity _ f) = do
+  generateFormPost $ renderTable $ faseAForm $ Just f
+
+ {-
+  widgetToPageContent $ [whamlet|
+     <form method=post action=@{FaseR} enctype=#{e}>
+       ^{w}
+       <button>Toggle
+  |]
+
+  -}
+--          ^{faseW}
+      -- <button>Submit
+--    <h1> Fases:
+--    <ul>
+--      $forall Entity _ f <- fases
+--        <li> Fase: #{faseNumber f}, Turno #{faseTurn f}, Válida: #{faseValid f}
+--        ^{renderFormFase f}
+
 
 getListR :: Handler Html
 getListR = do
-  professors  <- runDB $ selectList [] [Asc ProfessorName]
-  courses     <- runDB $ selectList [] [Asc CourseCode]
-  fases       <- runDB $ selectList [] [Asc FaseNumber, Asc FaseTurn]
+  professors     <- runDB $ selectList [] [Asc ProfessorName]
   (profW, p_enc) <- generateFormPost $ renderTable $ professorAForm
+  courses          <- runDB $ selectList [] [Asc CourseCode]
   (courseW, c_enc) <- generateFormPost $ renderTable $ courseAForm
+  fases <- runDB $ selectList [] [Asc FaseNumber, Asc FaseTurn]
+  f_ws  <- mapM generateFormFase fases
   defaultLayout $ do
     setTitle "Professors"
     $(widgetFile "professors" )
@@ -62,3 +83,16 @@ postSlotR =
      runDB $ deleteWhere ([] :: [Filter Fase])
      runDB $ mapM_ (\(n, t) -> insert_ $ Fase n t False) [(n, t) | n <- [1..10], t <- [1, 2]]
      return "Deleting all slots\n"
+
+postFaseR :: Handler Html
+postFaseR =
+  do
+    ((res, _), _) <- runFormPost $ renderTable $ faseAForm Nothing
+    case res of
+      FormSuccess f -> (runDB $ do
+        Just (Entity k v) <- getBy $ UniqueFase (faseNumber f) (faseTurn f)
+        replace k (Fase (faseNumber f) (faseTurn f) (not $ faseValid f))
+        )
+      _ -> print $ ("Error" :: Text)
+    redirect ListR
+
