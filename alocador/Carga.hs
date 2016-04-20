5e7ab066b798359b83d09d68efece97d6f40c008
Emilio Wuerges <wuerges@gmail.com>
2016-04-20 15:46:55 -0300

    alocando horarios

--- alocador/Carga.hs
+++ alocador/Carga.hs
@@ -70,10 +70,9 @@ manhaSeguinte :: Hor -> Hor -> Bool
 manhaSeguinte (Hor d1 h1) (Hor d2 h2) = d2 == d1 + 1 && h1 == 21 && h2 == 7
 
 mesmoDia :: Hor -> Hor -> Bool
-mesmoDia (Hor d1 h1) (Hor d2 h2) | d1 /= d2  = False
-                                 | otherwise = if h1 <= 12
-                                                  then h2 <= 12
-                                                  else h2 > 12
+mesmoDia (Hor d1 h1) (Hor d2 h2)
+  | d1 /= d2  = False
+  | otherwise = (h1 < 12 && h2 >= 12) || (h1 >= 12 && h2 < 12)
 
 consecutivos :: Hor -> Hor -> Bool
 consecutivos (Hor d1 h1) (Hor d2 h2) = d1 == d2 && ((h1 == 7 && h2 == 10) || (h1 == 19 && h2 == 21))
